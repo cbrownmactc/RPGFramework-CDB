3c3cf63e003d8afb498ff9563be5c273c5a8f760
cbrownmactc <cbrown@muskegonisd.org>
2026-01-12 11:37:13 -0500

    Added TelnetConnection async reading. Added checking for escape characters and backspace/delete.

--- Engine/PlayerNetwork.cs
+++ Engine/PlayerNetwork.cs
@@ -7,6 +7,7 @@ namespace RPGFramework
     internal class PlayerNetwork
     {
         public TcpClient Client { get; }
+        public TelnetConnection? TelnetConnection { get; set; }
         public StreamWriter Writer { get; }
         public StreamReader Reader { get; }
 
@@ -15,8 +16,17 @@ namespace RPGFramework
             Client = client;
            
             NetworkStream stream = client.GetStream();
-            Writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
-            Reader = new StreamReader(stream, Encoding.UTF8);
+
+            // TODO: Test if emojis work without UTF8
+            Writer = new StreamWriter(stream, new UTF8Encoding(false))
+            {
+                AutoFlush = true,
+                NewLine = "\r\n"
+            };
+
+            //Writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+            //Reader = new StreamReader(stream, Encoding.UTF8);
+            TelnetConnection = new TelnetConnection(stream, Encoding.UTF8);
         }
     }
 }
